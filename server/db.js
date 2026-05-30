2ffb1e630310843848f51ea697d4090626188887
Tuien18old <phamvantuyen170305@gmail.com>
2026-05-30 22:26:32 +0700

    prepare backend for render

--- server/db.js
+++ server/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-// Thêm chữ "bloghub" ngay trước dấu ?ssl=true
-const uri =
-  "mongodb://bloghub_user:12345677@ac-k6sfvmg-shard-00-00.8ftc67m.mongodb.net:27017,ac-k6sfvmg-shard-00-01.8ftc67m.mongodb.net:27017,ac-k6sfvmg-shard-00-02.8ftc67m.mongodb.net:27017/bloghub?ssl=true&replicaSet=atlas-l30jvw-shard-0&authSource=admin&appName=Cluster0";
+const uri = process.env.MONGO_URI;
 
 mongoose
   .connect(uri)
