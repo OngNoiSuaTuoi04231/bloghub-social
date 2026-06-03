77c02899796c2ec5d40bc44272905532bcd84c82
Trunghinni <nguyenthanhtrung21122005@gmail.com>
2026-06-03 10:14:19 +0700

    fix: add axios 401 interceptor to auto redirect to login on token expiry

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import axios from "axios";
 
-createRoot(document.getElementById('root')).render(
+axios.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = "/login";
+    }
+    return Promise.reject(err);
+  },
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
